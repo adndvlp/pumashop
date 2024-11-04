bb8a523c7b4eaab703b8492706fcf176a8488a50
adndvlp <165452363+adndvlp@users.noreply.github.com>
2024-11-03 21:37:09 -0600

    Update api.js

--- api.js
+++ api.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const app = express()
 const path = require('path')
 const { Auth, isAuthenticated, deleteUser } = require('./auth.controller')
-const port = 3000
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
